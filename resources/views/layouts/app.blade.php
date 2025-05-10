4d87e265d6fae235bc4ed17ed2dd44ea10671018
wid.o2 <wid.o2@live.com>
2025-05-10 23:40:03 +0700

    fix navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,17 +32,17 @@
         <div class="navbar-center">
             <ul class="menu  lg:menu-horizontal rounded-box">
                 <li>
-                    <a>
+                    <a wire:navigate href="{{ route('dashboard') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                         </svg>
-                        Inbox
-                        <span class="badge badge-xs">99+</span>
+                        Home
+                        {{-- <span class="badge badge-xs">99+</span> --}}
                     </a>
                 </li>
-                <li>
+                {{-- <li>
                     <a>
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
@@ -52,7 +52,7 @@
                         Updates
                         <span class="badge badge-xs badge-warning">NEW</span>
                     </a>
-                </li>
+                </li> --}}
                 <div class="dropdown ">
 
                     <li tabindex="0" role="button">
